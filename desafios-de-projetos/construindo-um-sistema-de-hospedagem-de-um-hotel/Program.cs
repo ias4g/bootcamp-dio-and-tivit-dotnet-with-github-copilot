c3c4428ad885dc95d12bac3e6139a0f9aef95694
Ias4g <izaell.oficial@gmail.com>
2026-01-19 21:50:01 -0300

    feat: Update project to target .NET 8.0 and enhance reservation logic

--- desafios-de-projetos/construindo-um-sistema-de-hospedagem-de-um-hotel/Program.cs
+++ desafios-de-projetos/construindo-um-sistema-de-hospedagem-de-um-hotel/Program.cs
@@ -8,9 +8,11 @@ List<Pessoa> hospedes = new List<Pessoa>();
 
 Pessoa p1 = new Pessoa(nome: "Hóspede 1");
 Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+Pessoa p3 = new Pessoa(nome: "Hóspede 3");
 
 hospedes.Add(p1);
 hospedes.Add(p2);
+hospedes.Add(p3);
 
 // Cria a suíte
 Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
@@ -23,3 +25,4 @@ reserva.CadastrarHospedes(hospedes);
 // Exibe a quantidade de hóspedes e o valor da diária
 Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
 Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+Console.WriteLine($"Dias reservados: {reserva.DiasReservados}");
